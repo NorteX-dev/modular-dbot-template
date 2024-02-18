715f76f4cb89d4d74aff0cf529cbb427a4ea7eb8
NorteX <contact@nortexdev.com>
2024-02-18 19:43:41 +0100

    before nuke module-specific schemas

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,31 +1,45 @@
 import yaml from "js-yaml";
 import path from "path";
-import { readFileSync } from "fs";
-import { debugLog, infoLog, severeLog } from "./logger";
+import { readFileSync, writeFileSync } from "fs";
+import { debugLog, severeLog } from "./logger";
 import { Module } from "./modules";
 import { ZodSchema, z } from "zod";
 import { logZodError } from "@nortex/pretty-zod-error";
 
-export const loadConfig = async (modules: Module[]) => {
-	try {
-		const yamlFile: any = yaml.load(readFileSync(path.join(__dirname, "../..", "./config.yml"), "utf-8"));
-		// const validated = configSchema.safeParse(yamlFile);
-		let schemaObj: Record<string, ZodSchema> = {};
-		for (let module of modules) {
-			if (!module.metadata.config) continue;
-			schemaObj[module.metadata.id] = module.metadata.config;
-			debugLog(`Loaded config schema for module ${module.metadata.id}`);
-		}
-		let schema = z.object(schemaObj);
+const coreSchema = {
+	token: z.string(),
+};
 
-		let result = schema.safeParse(yamlFile);
-		if (!result.success) {
-			severeLog("Fatal: Failed to parse configuration file. Errors:");
-			severeLog(logZodError(result.error));
-			process.exit(1);
-		}
+const zodCoreSchema = z.object(coreSchema);
+
+export type Config = any;
+
+export const loadConfig = async (modules: Module[]): Promise<Config> => {
+	let yamlFile: any;
+	try {
+		yamlFile = yaml.load(readFileSync(path.join(__dirname, "../..", "./config.yml"), "utf-8"));
 	} catch (err) {
 		severeLog("Fatal: config.yml is not a valid YAML file.");
 		process.exit(1);
 	}
+
+	let schemaObj: Record<string, ZodSchema> = {
+		...coreSchema,
+	};
+	for (let module of modules) {
+		if (!module.metadata.config) continue;
+		schemaObj[module.metadata.id] = module.metadata.config;
+		debugLog(`Loaded config schema for module ${module.metadata.id}`);
+	}
+	let schema = z.object(schemaObj);
+
+	let result = schema.safeParse(yamlFile);
+	if (!result.success) {
+		severeLog("Fatal: Failed to parse configuration file. Errors:");
+		severeLog(logZodError(result.error));
+		process.exit(1);
+	}
+
+	let config = result.data;
+	return config;
 };
--- src/core/index.ts
+++ src/core/index.ts
@@ -2,7 +2,7 @@ import { Client, IntentsBitField } from "discord.js";
 import { debugLog, infoLog, severeLog, welcome } from "./logger";
 import { createCommands, createComponents, createEvents } from "nhandler";
 import { Module, loadModules } from "./modules";
-import { loadConfig } from "./config";
+import { Config, loadConfig } from "./config";
 
 export let client = new Client({
 	intents: [IntentsBitField.Flags.GuildMembers],
@@ -19,9 +19,9 @@ componentHandler.on("debug", (...m) => debugLog(...m));
 export const init = async () => {
 	try {
 		welcome();
-		await client.login(process.env.DISCORD_BOT_TOKEN);
 		loadedModules = await loadModules();
-		await loadConfig(loadedModules);
+		const config: Config = await loadConfig(loadedModules);
+		await client.login();
 		infoLog(`Logged in as ${client.user?.username}!`);
 	} catch (err) {
 		severeLog(err);
